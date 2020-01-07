11fac09e9424026e158a243caa616b3720f44beb
Rich Vigorito <richv@mac-mb6GJG5J.local>
2020-01-07 14:51:24 -0800

    trying to packagize this
    https://dev.to/rf_schubert/how-to-create-a-pip-package-and-host-on-private-github-repo-58pa

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='switch-classes',
+    version='0.0.1',
+    description='Switch Classes',
+    url='git@github.com:richvigorito/switch-classes.git',
+    author='Rich Vigorito',
+    author_email='nicetry',
+    license='unlicense',
+    packages=['richvigorito'],
+    zip_safe=False
+)
+
 install_requires=['json']
